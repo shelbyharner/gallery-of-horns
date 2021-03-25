9ceb92417dfbda784ebc8d8deae8b4c19a8dcfc3
Shelby Harner <shelby.harner@gmail.com>
2021-03-24 23:24:48 -0700

    complete vote click state

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -6,26 +6,26 @@ class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      voteClick: 0
+      votes: 0
     }
   }
 
-  numOfVotes = () => {
-    this.setState({voteClick: this.state.voteClick + 1});
+  numOfClicks = () => {
+    this.setState({votes: this.state.votes + 1});
   }
 
   render() {
     return (
       <Card 
         style={{ width: '18rem' }}
-        onClick={this.numOfVotes}>
+        onClick={this.numOfClicks}>
         <Card.Img variant="top" src={this.props.img_url}/>
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
           <Card.Text>{this.props.description}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          😻 = {this.state.voteClick}
+          😻 = {this.state.votes}
         </Card.Footer>
       </Card>
     );
